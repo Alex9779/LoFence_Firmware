c5065358429df8920eeecc4cb8489097e0b6c50e
Alexander Leisentritt <alexander.leisentritt@alitecs.de>
2023-09-13 15:40:35 +0200

    reset default mearurement interval and update comment

--- LoFence-V2/main.h
+++ LoFence-V2/main.h
@@ -2,8 +2,8 @@
 #define MAIN_H_
 
 #define INTERVAL_SECONDS 5 * 60 // time to sleep between measurements
-#define RANDOMNESS 10 // +- time to sleep between measurements
-#define MEASURE_MS 100 // time in ms a measurement should take (per polarity)
+#define RANDOMNESS 10 // +- time to sleep between measurements in ms
+#define MEASURE_MS 6000 // time in ms a measurement should take (per polarity)
 
 void power_save(uint32_t sec);
 void log_serial(char *msg);
