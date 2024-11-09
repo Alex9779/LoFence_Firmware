e12c3f216cfeb7da5ecfbb0ca4daa6af23c32e46
Alexander Leisentritt <alexander.leisentritt@alitecs.de>
2024-11-09 18:47:55 +0100

    fix reading bat_low_min from eeprom

--- LoFence-V2/main.c
+++ LoFence-V2/main.c
@@ -589,7 +589,7 @@ void check_battery()
 {
 	// if maximum cycles the battery has been low is not reached
 	// and if the battery is above the absolute minimum of 3100mV
-	if (bat_low_count < eeprom_read_byte(&bat_low_count_max) && volt_bat > bat_low_min && volt_bat < eeprom_read_word(&bat_low))
+	if (bat_low_count < eeprom_read_byte(&bat_low_count_max) && volt_bat > eeprom_read_word(&bat_low_min) && volt_bat < eeprom_read_word(&bat_low))
 	{
 		bat_low_count++;
 		
@@ -597,7 +597,7 @@ void check_battery()
 		log_serial(buffer_info);
 	}
 	// if battery is lower than absolute minimum
-	else if (volt_bat <= bat_low_min)
+	else if (volt_bat <= eeprom_read_word(&bat_low_min))
 	{
 		log_serial_P(PSTR("Battery lower than absolute minimum, deactivating next cycle!\r\n"));
 		
